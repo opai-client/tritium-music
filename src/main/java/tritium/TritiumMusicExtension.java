d0a0c64e74593eafde25dc25c639593e1e8b59fc
IzumiKonata <izumiikonatta@gmail.com>
2026-04-03 23:34:01 +0800

    bump version

--- src/main/java/tritium/TritiumMusicExtension.java
+++ src/main/java/tritium/TritiumMusicExtension.java
@@ -25,7 +25,7 @@ public class TritiumMusicExtension {
 
     public static final String NAME = "Tritium Music";
     public static final String AUTHOR = "IzumiiKonata";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @Getter
     private static final TritiumMusicExtension instance = new TritiumMusicExtension();
